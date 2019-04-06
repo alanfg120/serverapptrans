b5cdde04d228b35b1c401853c026643bef1865bf
Alan <Alanfg@MacBook-Pro-de-Alan.local>
2019-04-06 07:32:57 -0500

    resolves mongo

--- resolvers.js
+++ resolvers.js
@@ -1,13 +1,21 @@
-var resolvers = { 
-    hello: () => "Hello world!",
-    usuario: () => {
-       return {
-           id:"12",
-           name:"alan",
-           lastname:"hola"
-          }
-        }
+var jwt = require("jsonwebtoken");
+var expressJwt = require("express-jwt");
+var jwtClave = "adsgkgsf";
+var MongoClient = require("mongodb").MongoClient;
+const { ObjectId } = require("mongodb")
+const dbName = "apptrans"
+const url = 'mongodb://localhost:27017';
+
+
+var resolvers = {
+  hello: () => "Hello world!",
+  usuario: () => {
+    return {
+      id: "12",
+      name: "alan",
+      lastname: "hola"
+    };
+  }
 };
 
-
-module.exports=resolvers
+module.exports = resolvers;
