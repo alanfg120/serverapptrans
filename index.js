ee69528b1f16825e52376f66249ad8b8cfeb451b
Alan <Alanfg@MacBook-Pro-de-Alan.local>
2019-04-05 22:18:35 -0500

    grahpql

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var cors = require('cors');
 var jwt=require('jsonwebtoken');
 var expressJwt=require('express-jwt');
 var router = require("./rest/rest");
-
+var graphqlHTTP = require('express-graphql');
 var jwtClave="adsgkgsf";
 var app = express();
 
@@ -14,8 +14,10 @@ app.use(express.urlencoded());
 app.use(express.json())
 app.use(expressJwt({secret:jwtClave}).unless({path: ["/login"]}));
 app.use(router);
-
-
+app.use('/graphql', graphqlHTTP({
+  rootValue: root,
+  graphiql: true,
+}));
 
  
 
