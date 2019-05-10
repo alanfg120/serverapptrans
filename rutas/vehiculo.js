85a01ca7bbad67984daad0064ceae622464594ac
alan <alanfg120@gmail.com>
2019-05-09 22:03:37 -0500

    vehiculo

--- rutas/vehiculo.js
+++ rutas/vehiculo.js
@@ -38,24 +38,107 @@ router.post("/upload", fileUpload(), (req, res) => {
     client.close();
   });
 });
-
 router.post("/new", (req, res) => {
   console.log(req.body);
   MongoClient.connect(url, (err, clt) => {
     const db = clt.db(dbName);
-    db.collection("vehiculos").insertOne(req.body, (err, rst) => {
+    db.collection("vehiculos").insertOne(req.body,{ useNewUrlParser: true }, (err, rst) => {
       if (err) {
-        console.log(err);
-
-        res.status(400).send({ error: true });
-      } else res.status(200).send({ error: false });
+        res.send({ error: true });
+      } else res.send({ status: true });
     });
     clt.close();
   });
 
   
 });
+router.get("/get", (req, res) => {
+  MongoClient.connect(url, (err, clt) => {
+    const db = clt.db(dbName);
+    db.collection("vehiculos")
+      .find()
+      .toArray()
+      .then(data => {
+        if (data) res.status(200).send(data);
+      })
+      .catch(err => res.status(400).send(err));
+    clt.close();
+  });
+});
+router.delete("/delete/:vehiculo", (req, res) => {
+  console.log(req.params);
 
+  MongoClient.connect(url, (err, clt) => {
+    const db = clt.db(dbName);
+    db.collection("vehiculos").deleteOne(
+      { _id: ObjectId(req.params.vehiculo) },
+      (err, rsl) => {
+        if (err) res.status(400).send({ error: true });
+        else res.status(200).send({ error: false });
+      }
+    );
+  });
+});
+router.get("/get/:placa", (req, res) => {
+  console.log(req.params);
 
+  MongoClient.connect(url, (err, clt) => {
+    const db = clt.db(dbName);
+    db.collection("vehiculos")
+      .find({ placa: req.params.placa })
+      .toArray()
+      .then(data => {
+        if (data) res.status(200).send(data[0]);
+      })
+      .catch(err => res.status(400).send(err));
+    clt.close();
+  });
+});
+router.put("/update", (req, res) => {
+ 
 
+  let _id = req.body._id;
+  delete req.body._id;
+  MongoClient.connect(url, function(err, client) {
+    const db = client.db(dbName);
+    db.collection("vehiculos").findOneAndUpdate(
+      { _id: ObjectId(_id) },
+      { $set: req.body },
+      (err, rsl) => {
+        if (err) res.status(400).send({ error: true });
+        else res.status(200).send({ error: false });
+      }
+    );
+
+    client.close();
+  });
+});
+router.post("/upload/update", fileUpload(), (req, res) => {
+  console.log(req.files);
+  
+  if (req.files) {
+    Object.values(req.files).forEach(file => {
+      file.mv(
+        `Documentos/Vehiculos/${req.body.placa}/${file.name}`,
+        err => {}
+      );
+    });
+    res.send({ status: true });
+  } else res.send({ error: true });
+});
+router.put("/validar", (req, res) => {
+  
+  MongoClient.connect(url, (err, clt) => {
+    const db = clt.db(dbName);
+    db.collection("vehiculos").updateOne(
+      { _id: ObjectId(req.body._id) },
+      { $set: { valido: req.body.valid } },
+      (err,rsl)=>{
+        if(err)res.status(400).send({error:true})
+        else res.status(200).send({error:false})
+      }
+    );
+    clt.close()
+  });
+});
 module.exports = router;
