ae0ad7a7341f0cf4dc150387030fc3467aebf8d0
alan <alanfg120@gmail.com>
2019-05-16 17:27:21 -0500

    free listo

--- index.js
+++ index.js
@@ -10,10 +10,11 @@ var contrato=require("./rutas/contrato")
 var vehiculo=require("./rutas/vehiculo")
 var trailer=require("./rutas/trailer")
 app.use(cors());
-app.use(express.static('asset'));
+app.use(express.static(__dirname + '/dist'));
+app.use('/asset', express.static(__dirname + '/static'));
 app.use(express.urlencoded());
 app.use(express.json())
-app.use(expressJwt({secret:jwtClave}).unless({path: ["/usuario/login"]}));
+app.use(expressJwt({secret:jwtClave}).unless({path: ["/usuario/login","/dist"]}));
 app.use( (err, req, res, next)=> {
   if (err.name === 'UnauthorizedError') {
     res.status(401).send('Unauthorized');
@@ -25,7 +26,7 @@ app.use('/contrato',contrato);
 app.use('/vehiculo',vehiculo);
 app.use('/trailer',trailer);
 
-
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYWxhbmZnMTIwQGdtYWlsLmNvbSIsImlhdCI6MTU1ODA0NDYzNH0.SxfUBnyV9Tpl8gQy0EM7KCOHLT-3q5BtB-BF48qXl1Q
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
